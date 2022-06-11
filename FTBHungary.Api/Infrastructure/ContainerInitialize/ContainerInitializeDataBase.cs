1842573bd9e24fbdaa35e5edc71d3a68e4fa9d48
KemenyMatyi <71182917+KemenyMatyi@users.noreply.github.com>
2022-06-11 16:23:31 +0200

    git ignore (#1)

--- FTBHungary.Api/Infrastructure/ContainerInitialize/ContainerInitializeDataBase.cs
+++ FTBHungary.Api/Infrastructure/ContainerInitialize/ContainerInitializeDataBase.cs
@@ -1,10 +1,16 @@
 namespace FTBHungary.Api.Infrastructure.ContainerInitialize;
 
+using DataAccess;
+using Microsoft.EntityFrameworkCore;
+
 public static class ContainerInitializeDataBase
 {
     public static void Init(IServiceCollection services, IConfiguration config)
     {
-
+        var serverVersion = new MySqlServerVersion(new Version(8, 0, 27));
+        services.AddDbContext<FTBHungaryContext>(
+            dbContextOptions => dbContextOptions
+                .UseMySql(config.GetConnectionString("DefaultConnection"), serverVersion));
     }
 }
 
