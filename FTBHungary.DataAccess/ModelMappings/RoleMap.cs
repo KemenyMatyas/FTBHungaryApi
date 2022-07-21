5b30e772da2b76bf122579d75205a2e3ccaae4c1
KemenyMatyi <kemeny.matyi@gmail.com>
2022-07-21 13:32:01 +0200

    added new roles with guid

--- FTBHungary.DataAccess/ModelMappings/RoleMap.cs
+++ FTBHungary.DataAccess/ModelMappings/RoleMap.cs
@@ -9,5 +9,11 @@ public class RoleMap :  IEntityTypeConfiguration<Role>
     public void Configure(EntityTypeBuilder<Role> builder)
     {
         builder.HasKey(k => k.Guid);
+
+        var list = new List<Role>();
+        list.Add(new Role(){Name = "Admin"});
+        list.Add(new Role(){Name = "User"});
+        
+        builder.HasData(list);
     }
 }
